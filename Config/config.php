e62566178fe75331ac269640be1a8095d6c1e301
Anndres <silvabasauri@gmail.com>
2024-08-07 22:28:06 -0500

    Se crea ambiente automatico

--- Config/config.php
+++ Config/config.php
@@ -1,13 +1,28 @@
 <?php
-    define("HOST",'localhost');
-    //define("DB_USER",'legrape_comparti');
-    define("DB_USER",'root');
-    //define("DB_PASS",'27+w){SmQ$vm'); 
-    define("DB_PASS",'');
-    //define("DB_NAME",'legrape_bendey');
-    define("DB_NAME",'bendey');
+// Detectar el entorno automáticamente
+if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1') {
+    // Entorno de desarrollo
+    define("ENVIRONMENT", "development");
+} else {
+    // Entorno de producción
+    define("ENVIRONMENT", "production");
+}
 
-    define("PORT",3306);
-    define("CHARSET",'utf8');
-    //define("UNIX_SOCKET",'/var/run/mysql/mysql.sock');
-    define("SYSTEMNAME","Bendey");
+// Configuración específica de cada entorno
+if (ENVIRONMENT == 'development') {
+    define("HOST", 'localhost');
+    define("DB_USER", 'root');
+    define("DB_PASS", '');
+    define("DB_NAME", 'bendey');
+    define("API_KEY", 'your_development_api_key');
+} else {
+    define("HOST", 'localhost');
+    define("DB_USER", 'u274409976_japipos');
+    define("DB_PASS", 'Redes2804751$$$');
+    define("DB_NAME", 'u274409976_japipos');
+    define("API_KEY", 'your_production_api_key');
+}
+
+define("PORT", 3306);
+define("CHARSET", 'utf8');
+define("SYSTEMNAME", "Bendey");
